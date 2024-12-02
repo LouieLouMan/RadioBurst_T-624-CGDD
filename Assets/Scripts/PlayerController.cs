bd1c544f941ed763ddec8286655eb2325e6ed107
Kristján Orri <koli170@outlook.com>
2024-12-02 13:40:33 +0000

    bounds check added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public UnityEngine.Vector2 levelBounds;
     public Transform movePoint;
     // Start is called before the first frame update
     void Start()
@@ -16,17 +17,37 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        float pointDirection_x = 0f;
+        float pointDirection_y = 0f;
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            pointDirection_x = Input.GetAxisRaw("Horizontal");
+            pointDirection_y = Input.GetAxisRaw("Vertical");
+            if (Mathf.Abs(pointDirection_x) == 1f)
             {
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"),0,0);
-                transform.eulerAngles = new Vector3(0.0f, 0.0f, -90f * Input.GetAxisRaw("Horizontal"));
+                if (Mathf.Abs(movePoint.position.x) == levelBounds.x){
+                    if (pointDirection_x > 0 && movePoint.position.x == levelBounds.x){
+                        pointDirection_x = -pointDirection_x;
+                    }
+                    if (pointDirection_x < 0 && movePoint.position.x == -levelBounds.x){
+                        pointDirection_x = -pointDirection_x;
+                    }
+                }
+                movePoint.position += new Vector3(pointDirection_x,0,0);
+                transform.eulerAngles = new Vector3(0.0f, 0.0f, -90f * pointDirection_x);
             }
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            if (Mathf.Abs(pointDirection_y) == 1f)
             {
-                movePoint.position += new Vector3(0,Input.GetAxisRaw("Vertical"),0);
-                if (Input.GetAxisRaw("Vertical") > 0){
+                if (Mathf.Abs(movePoint.position.y) == levelBounds.y){
+                    if (pointDirection_y > 0 && movePoint.position.y == levelBounds.y){
+                        pointDirection_y = -pointDirection_y;
+                    }
+                    if (pointDirection_y < 0 && movePoint.position.y == -levelBounds.y){
+                        pointDirection_y = -pointDirection_y;
+                    }
+                }
+                movePoint.position += new Vector3(0,pointDirection_y,0);
+                if (pointDirection_y > 0){
                     transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
                 }
                 else 
