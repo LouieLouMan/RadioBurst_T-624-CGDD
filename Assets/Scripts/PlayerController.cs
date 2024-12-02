4721bf2cd944cca04512e313ab01c51667f477e4
Kristján Orri <koli170@outlook.com>
2024-12-02 15:25:36 +0000

    clean up code a tiny bit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,12 +17,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        float pointDirection_x = 0f;
-        float pointDirection_y = 0f;
+
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            pointDirection_x = Input.GetAxisRaw("Horizontal");
-            pointDirection_y = Input.GetAxisRaw("Vertical");
+            float pointDirection_x = Input.GetAxisRaw("Horizontal");
+            float pointDirection_y = Input.GetAxisRaw("Vertical");
             if (Mathf.Abs(pointDirection_x) == 1f)
             {
                 if (Mathf.Abs(movePoint.position.x) == levelBounds.x){
