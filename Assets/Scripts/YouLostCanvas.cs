6b90c1c27b6c5db22c04d7db6e7c23f20d1df58f
Marteinn23 <marteinn23@ru.is>
2024-12-03 19:50:10 +0000

    made the YouLost menu, & made it as functional as i can make it right now i think.

--- Assets/Scripts/YouLostCanvas.cs
+++ Assets/Scripts/YouLostCanvas.cs
@@ -7,8 +7,8 @@ public class MainCanvasScript : MonoBehaviour
 {
     GameObject child;
 
-    int keyDownDuration = 300;
-    int keyDownCount = 300;
+    public int keyDownDuration = 300;
+    int keyDownCount = 0;
 
     void Start()
     {
@@ -45,21 +45,29 @@ public class MainCanvasScript : MonoBehaviour
         child.gameObject.SetActive(true);
     }
 
-    public void ResumeGame()
+    void ResumeGame()
     {
         // Set Time.timeScale back to 1 to resume gameplay
         Time.timeScale = 1;
         // Hide PauseMenu panel (deactivate its gameObject)
         child.gameObject.SetActive(false);
     }
-
-    public void MainMenu ()
+    
+    public void RetryGame()
     {
+        ResumeGame();
+        //TODO impliment retry game
+        Debug.Log("NOT YET IMPLIMENTED, CANNOT RETRY GAME.");
+    }
+    public void MainMenu ()
+    {   
+        ResumeGame();
         SceneManager.LoadScene(0);
     }
     // Called when we click the "Quit" button.
     public void OnQuitButton ()
-    {
+    {   
+        ResumeGame();
         Application.Quit();
     }
 
