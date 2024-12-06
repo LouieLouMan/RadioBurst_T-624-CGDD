7229d9e64d1d898e93ac5b4b316e3cfced0b78c3
teitur-23 <147160730+teitur-23@users.noreply.github.com>
2024-12-06 11:26:42 +0000

    build settings and spring cleaning

--- Assets/Scripts/LaserShooter.cs
+++ Assets/Scripts/LaserShooter.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using System.IO;
-using TMPro;
 
 public class LaserShooter : MonoBehaviour
 {
