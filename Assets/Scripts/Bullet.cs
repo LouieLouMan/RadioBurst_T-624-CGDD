a929312dd5ce164bd30dfb56556c87fc145d93f2
Marteinn23 <marteinn23@ru.is>
2024-12-09 16:01:10 +0000

    made it so that the bullet explodes into particles.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -5,11 +7,13 @@ public class Bullet : MonoBehaviour
     public float speed;
     public Rigidbody2D body;
     public UnityEngine.Vector2 direction;
-    
+
+    ParticleSystem ps;
     // Start is called before the first frame update
     void Start()
     {
        body.velocity = direction.normalized * speed;
+       ps = GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -19,6 +23,15 @@ public class Bullet : MonoBehaviour
     }
 
     void OnTriggerEnter2D(Collider2D other){
+        StartCoroutine(JuicyExplode());
+    }
+
+    IEnumerator JuicyExplode()
+    {
+            body.velocity = direction.normalized * 0;
+            ps.Play();
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            yield return new WaitForSeconds(1);
             Destroy(gameObject);
 
     }
