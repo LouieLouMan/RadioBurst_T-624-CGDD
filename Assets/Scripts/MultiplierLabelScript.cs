e0eb4ec837384738ffbcc7abed9a4024a3a13abd
Kristján Orri <koli170@outlook.com>
2024-12-09 15:41:09 +0000

    fire added

--- Assets/Scripts/MultiplierLabelScript.cs
+++ Assets/Scripts/MultiplierLabelScript.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MultiplierLabelScript : MonoBehaviour
@@ -7,9 +9,20 @@ public class MultiplierLabelScript : MonoBehaviour
     public TMPro.TextMeshProUGUI scoreLabel;
 
     float multiplier;
-    float last_mult = -1f;
+    float last_mult = 0f;
+
+    public ParticleSystem particles;
 
     public Animator _animator;
+    public Transform body;
+
+    void Start() 
+    {
+        var pmain = particles.main;
+        pmain.maxParticles = 0;
+        var pem = particles.emission;
+        pem.rateOverTime = 5;
+    }
     
     void Update()
     {
@@ -18,6 +31,20 @@ public class MultiplierLabelScript : MonoBehaviour
         if (multiplier != last_mult) {
             _animator.SetTrigger("Pop");
             last_mult = multiplier;
+        }
+        if (multiplier >= 3)
+        {
+            var pmain = particles.main;
+            pmain.maxParticles = (int)(multiplier * multiplier * multiplier);
+            var pem = particles.emission;
+            pem.rateOverTime = multiplier * multiplier * multiplier / 10;
+        }
+        else 
+        {
+            var pmain = particles.main;
+            pmain.maxParticles = 0;
+            var pem = particles.emission;
+            pem.rateOverTime = 5;
         }
         scoreLabel.text = multiplier.ToString("F1") + "X";
     }
